4a9bf24d494739cc4afc5e470602d2c157ea3295
Stephen Delaney <swdelaney2@gmail.com>
2015-12-04 09:21:23 -0600

    cleaned up files, deleted old code

--- controllers/account.rb
+++ controllers/account.rb
@@ -1,52 +1,18 @@
 class AccountController < ApplicationController
 
-  # ActiveRecord::Base.establish_connection(
-  #   :database => 'shopping_account',
-  #   :adapter => 'postgresql'
-  # )
-
-  # enable :sessions
-  #
-  # # CHECK IF USERNAME ALREADY EXISTS
-  # def does_user_exist(username)
-  #   user = Account.find_by(:user_name => username)
-  #    #Note to self -- this is not searching params, this searching the database for the :user_name column
-  #   if user #does it exist?
-  #     return true
-  #   else
-  #     return false
-  #   end
-  # end
-  #
-  # # AUTHORIZATION CHECK - FOR ALL PROTECTED PAGES
-  # def authorization_check
-  #   if session[:current_user] == nil
-  #     redirect '/not_authorized'
-  #   else
-  #     return true
-  #   end
-  # end
-
 get '/register' do
   erb :register
 end
 
 post '/register' do
-  # p params
   if does_user_exist(params[:user_name]) == true
     @taken = 'Sorry, that username has already been taken.'
 return erb :register
-    # return erb :taken
 end
 user = Account.create(user_email: params[:user_email], user_name: params[:user_name], password: params[:password])
-# p user
 session[:current_user] = user
 redirect '/'
 end
-
-# get '/taken' do
-#   erb :taken
-# end
 
 get '/login' do
   erb :login
@@ -73,10 +39,6 @@ get '/logout' do
   erb :logged_out
 end
 
-
-
-
-
 get '/success_login' do
   erb :success_login
 end
@@ -88,34 +50,5 @@ end
 get '/not_authorized' do
   erb :not_authorized
 end
-
-
-
-
-##DELETE?
-
-# get '/' do
-#   authorization_check
-# @user_name = session[:current_user].user_name
-#   @item = Item.all
-#   erb :index
-# end
-
-
-
-
-
-
-
-
-
-
-
-  # set :views, File.expand_path('../../views', __FILE__)
-  # set :public, File.expand_path('../../public', __FILE__)
-  #
-  # not_found do
-  #  erb :not_found
-  # end
 
 end
--- db/migrations.sql
+++ db/migrations.sql
@@ -1,3 +1,7 @@
 CREATE DATABASE shopping;
 \c shopping;
 CREATE TABLE items (id SERIAL PRIMARY KEY, name VARCHAR(255), quantity INTEGER);
+
+CREATE TABLE accounts (id SERIAL PRIMARY KEY, user_name VARCHAR(255), user_email VARCHAR(255), password_digest VARCHAR(255), is_admin BOOLEAN);
+\dt;
+-- SELECT * FROM accounts;
